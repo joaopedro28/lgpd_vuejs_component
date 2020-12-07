16969a495a57d7f7e4430b46e0145ecc587b2b58
Joao Pedro Rocha <joaopedro@wdhouse.com.br>
2020-12-07 10:07:13 -0300

    fix: change variables names

--- Lgpd.vue
+++ Lgpd.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <div class="cookies box" v-show="!box">
+        <div class="cookies box" v-show="box">
             <div v-if="config_cookies" class="content">
                 <div class="title">Controle sua privacidade</div>
                 <div class="text">Utilizamos cookies com objetivo de prover a melhor experiência no uso de nossos serviços. Ao clicar em “Aceitar”, concorda com a utilização de TODOS os cookies.</div>
                 <div class="links">
-                    <a class="pointer">Politica de privacidade</a>
+                    <a href="/politica-de-privacidade" >Política de privacidade</a>
                 </div>
                 <div class="flex-row buttons">
                     <div class="gray pointer" v-on:click="config_cookies = false, cookies_options = true">
@@ -74,10 +74,10 @@
                 </div>
                 <div class="flex-row">
                     <div class="links">
-                        <a class="pointer">Politica de privacidade</a>
+                        <a href="/politica-de-privacidade" >Política de privacidade</a>
                     </div>
                     <div class="links ">
-                        <a class="pointer">Politicas dos cookies</a>
+                        <a href="/politica-de-privacidade" >Política dos cookies</a>
                     </div>
                 </div>
                 <div class="buttons-config">
@@ -89,7 +89,7 @@
         </div>
     </div>
 </template>
-<style scoped src="./css/cookies.css" lang="css"></style>
+<style scoped src="./css/lgpd.css" lang="css"></style>
 <script>
 import Cookies from 'js-cookie';
 
@@ -102,29 +102,24 @@ export default {
             essenciais:true,
             marketing:true,
             funcionais:true,
-            box:true
+            box:false
         }
     },
     mounted() {
         setTimeout(() => {
-            if (Cookies.get('lgpd') == 'true' || sessionStorage.getItem("session") == 'true'){
-                this.box = true
-            }
-            else {
+            this.box = true;
+            if (Cookies.get('lgpd') == 'true' || sessionStorage.getItem("lgpd_config") == 'true'){
                 this.box = false
             }
         }, 3000);
     },
     methods: {
         SaveLGPD() {
-            sessionStorage.setItem("session", false)
-            Cookies.set('funcionais_cookie', this.funcionais )
-            Cookies.set('gtm_cookie', this.marketing )
-            if (this.marketing == true) {
-                sessionStorage.setItem("session", true)
-            }
-            if (this.funcionais == true) {
-                sessionStorage.setItem("session", true)
+            sessionStorage.setItem("lgpd_config", false)
+            Cookies.set('lgpd_load_functional', this.funcionais )
+            Cookies.set('lgpd_load_marketing', this.marketing )
+            if (this.marketing == true || this.funcionais == true) {
+                sessionStorage.setItem("lgpd_config", true)
             }
             if (this.marketing == true && this.funcionais == true) {
                 Cookies.set('lgpd', true)
